87c159f90d462bee35ea49f9365ffd12e24e053e
unknown <Mohammed>
2022-05-10 16:19:45 +0530

    last container

---
+++ lib/clipper/side_cut_clipper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class SideCutClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+
+    // TODO: implement getClip
+    throw UnimplementedError();
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    throw UnimplementedError();
+  }
+
+}
